301f9855ac07711cde1e69c68fe392b46160a83d
Phí Dương Đạt <hn447488@gmail.com>
2025-09-18 09:00:28 +0700

    update hello

--- src/Begginer.java
+++ src/Begginer.java
@@ -1,5 +1,7 @@
 public class Begginer {
     public static void main(String[] args) {
-        System.out.print("Hi beginner");
+        System.out.println("Hi beginner");
+        int a = 1;
+        System.out.print("Hi beginner 2");
     }
 }
